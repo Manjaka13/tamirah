4267e32bce38716094b4210821f8901dd4f4f74c
Hari <manjaka.rajaonson@gmail.com>
2022-04-07 19:07:55 +0300

    add cover

---
+++ components/Cover.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*
+    Home page cover
+*/
+
+const Cover = () => (
+	<div className="cover w-100 h-100 o-h">
+		<div className="cover__content w-100">
+			<div className="cover__overlay w-100 h-100"></div>
+		</div>
+	</div>
+);
+
+export default Cover;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import Heading from "components/Heading";
 import Navigation from "components/Navigation/Navigation";
 import Footer from "components/Footer/Footer";
+import Cover from "components/Cover";
 
 /*
 	Home page
@@ -16,6 +17,7 @@ const HomePage = () => (
 		/>
 		<main className="w-100">
 			<Navigation />
+			<Cover />
 			<Footer />
 		</main>
 	</Fragment>
